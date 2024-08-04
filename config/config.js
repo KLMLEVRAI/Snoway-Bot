81c7cb0877d2fcab8861b9b701634fa257e31c60
KLM <122469699+KLMLEVRAI@users.noreply.github.com>
2024-08-04 19:29:28 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   token: "Put-Token-Here",
-  botId: "Bot-Id",
-  buyers: ["276705058370879489"],
-  prefix: "+",
+  botId: "1269688994728251422",
+  buyers: ["658657618520440832"],
+  prefix: "-",
   color: "#2b2d31",
 };
